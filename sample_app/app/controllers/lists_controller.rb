7be28799bacefbff2dc234af6b6ae3967eb212f0
ReoHashiba <133374579+ReoHashiba@users.noreply.github.com>
2023-05-30 11:58:17 +0000

    [Finish] chapter4-2

--- sample_app/app/controllers/lists_controller.rb
+++ sample_app/app/controllers/lists_controller.rb
@@ -1,6 +1,14 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
+  
+  def create
+    list = List.new(list_params) #データを受け取り新規登録するためのインスタンス作成
+    list.save #データをDBに保存するためのsaveメソッド実行
+    redirect_to '/top' #top画面へ遷移
+  end
+    
 
   def index
   end
@@ -9,5 +17,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
