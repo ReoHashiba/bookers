d1fe34365f7f858c173d649c7a003be96f6fcfa1
ReoHashiba <133374579+ReoHashiba@users.noreply.github.com>
2023-06-01 15:09:57 +0000

    [Finish] chapter4-8

--- sample_app/app/controllers/lists_controller.rb
+++ sample_app/app/controllers/lists_controller.rb
@@ -4,9 +4,12 @@ class ListsController < ApplicationController
   end
   
   def create
-    list = List.new(list_params) #データを受け取り新規登録するためのインスタンス作成
-    list.save #データをDBに保存するためのsaveメソッド実行
-    redirect_to list_path(list.id) #top画面へ遷移
+    @list = List.new(list_params) #データを受け取り新規登録するためのインスタンス作成
+    if @list.save
+      redirect_to list_path(@list.id) #top画面へ遷移
+    else
+      render :new 
+    end
   end
     
 
