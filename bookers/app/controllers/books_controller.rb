cd04fce68c4e7f6e1b70d232f87ea59bea6e3bff
ReoHashiba <133374579+ReoHashiba@users.noreply.github.com>
2023-06-05 12:59:20 +0000

    [Update] create失敗時、update失敗時にエラーメッセージを表示させる機能追加

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,13 +1,12 @@
 class BooksController < ApplicationController
   
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
        flash[:notice] = "Book was successfully created."
-       redirect_to book_path(book.id)
+       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      @book = Book.new
       render :index
     end
   end
@@ -26,16 +25,19 @@ class BooksController < ApplicationController
   end 
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id) #show画面へ
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id) #show画面へ
+    else
+      render :edit
+    end
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books/index'
+    redirect_to '/books'
   end
   
   private
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -3,17 +3,19 @@ Rails.application.routes.draw do
   
   #[top]URLにアクセスされたら[homes]controllerの[top]アクションを呼び出す
   #----homes----
+  root to: 'homes#top'
   get '/top' => 'homes#top'
+  resources :books
   
   #----homes----
   
   #----books----
-  post 'books' => 'books#create'
-  get 'books/index' => 'books#index', as: 'index_book'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  # post 'books' => 'books#create'
+  # get 'books/index' => 'books#index', as: 'index_book'
+  # get 'books/:id' => 'books#show', as: 'book'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   #----books----
   
 end
